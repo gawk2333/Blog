c27a9471ad5f156e10aad90b668c6a8e1d3ec94b
Gao Kang <gaokang@Gaos-MacBook-Pro.local>
2021-10-28 21:53:14 +1300

    main page bg img

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import Home from './Home'
 import Fruit from './Fruit'
 
 const App = () => {
   return (
     <>
-      <h1>Hola! Fruit time!</h1>
-      <Fruit />
+      {/* <Fruit /> */}
+      <Home/>
     </>
   )
 }
